4ed3042bc919b040ee8469bfd4ea9ab65330fd15
MuraliMR259 <81211723+MuraliMR259@users.noreply.github.com>
2021-05-06 17:08:07 +0530

    Add files via upload

---
+++ day10-a.py
@@ -0,0 +1,6 @@
+s=list(map(int,input("Enter the value :").split(",")))
+count=0
+for i in s:
+    count+=1
+print(count)
+print(len(s))
---
+++ day10.py
@@ -0,0 +1,4 @@
+s=list(map(int,input("Enter the value :").split(",")))
+l=sorted(s)
+print(l)
+print(l[len(l)-2])
