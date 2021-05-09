79e71e651ce2af59ea97f42cfebddf934dd49670
MuraliMR259 <81211723+MuraliMR259@users.noreply.github.com>
2021-05-09 22:06:00 +0530

    Add files via upload

---
+++ day13-a.py
@@ -0,0 +1,11 @@
+#num=input("enter the number :")
+#sum=0
+#for i in num:
+#    sum+=int(i)
+#print(sum)
+
+num=input("enter the number :")
+l=[]
+for i in num:
+    l.append(int(i))
+print(sum(l))
---
+++ day13.py
@@ -0,0 +1,15 @@
+#number=int(input("enthr the num :"))
+#if(number < 0):
+#    print("this num negative")
+#else:
+#   print("numbers is positive")
+
+    
+    
+num=input("enter the number:")
+l=list(num)
+if '-' in num:
+     print("this num negative")
+else:
+    print("numbers is positive")
+    
